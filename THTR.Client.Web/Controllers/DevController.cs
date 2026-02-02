457394ad7306e066390a7cb8291c06634b5a25af
John Ploskina <johnploskina@gmail.com>
2026-02-01 20:23:23 -0500

    building animations and player avatar

---
+++ THTR.Client.Web/Controllers/DevController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace THTR.Client.Web.Controllers
+{
+    [ApiController]
+    [Route("Dev")]
+    public class DevController : Controller
+    {
+        [HttpGet("EditAvatarJson")]
+        public IActionResult EditAvatarJson()
+        {
+            return View();
+        }
+    }
+}
