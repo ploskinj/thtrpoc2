9473c59e5c116ea8a6ed4c80b82749e5f286a4b0
ploskinj <johnploskina@gmail.com>
2026-02-07 09:43:30 -0500

    game state

---
+++ THTR.Client.Web/THTR/POC/GameState.cs
@@ -0,0 +1,48 @@
+﻿using MessagePack;
+
+namespace THTR.Client.Web.THTR.POC
+{
+    public class POCGameState
+    {
+        List<PocPlayer> players = new List<PocPlayer>();
+    }
+
+    [MessagePackObject]
+    public class PocPlayer
+    {
+        [Key(0)]
+        public int player_id { get; set; }
+        [Key(1)]
+        public int map_x { get; set; }
+        [Key(2)]
+        public int map_y { get; set; }
+        [Key(3)]
+        public int velocity_x { get; set; }
+        [Key(4)]
+        public int velocity_y { get; set; }
+        [Key(5)]
+        public int state_elapsed_ms { get; set; }
+        [Key(6)]
+        public PLAYERSTATES state { get; set; }
+        [Key(7)]
+        public DIRECTIONS direction { get; set; }
+    }
+    
+
+
+    public enum PLAYERSTATES
+    {
+        IDLE1 = 0, 
+        IDLE2 = 1, 
+        WALK = 2, 
+        ATTACK = 3        
+    }
+
+    public enum DIRECTIONS
+    {
+        NORTH = 0, 
+        EAST = 1, 
+        SOUTH = 2, 
+        WEST = 3
+    }
+}
---
+++ THTR.Client.Web/THTR/POC/POCDirector.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Concurrent;
+using THTR.Client.Web.Hubs;
+
+namespace THTR.Client.Web.THTR.POC;
+
+// house manager is what runs the connect/disconnect, list of players, 
+public class PocDirector
+{
+    private readonly IHubContext<POCHub> _hub_context;
+    private readonly ConcurrentDictionary<string, PocPlayer> _players;
+    private Timer? _tick_timer;
+
+    public PocDirector(IHubContext<POCHub> hub_context)
+    {
+        _hub_context = hub_context;
+        _players = new ConcurrentDictionary<string, PocPlayer>();
+        start_tick();
+    }
+
+    private void start_tick()
+    {
+        _tick_timer = new Timer(async _ => await tick(), null, TimeSpan.Zero, TimeSpan.FromMilliseconds(50));
+    }
+
+    public async Task tick()
+    {
+        var game_state = new
+        {
+            player_count = _players.Count,
+            timestamp = DateTime.UtcNow,
+            players = _players.Values
+        };
+
+        await _hub_context.Clients.All.SendAsync("receive_game_state", game_state);
+    }
+
+    public void enter(string connection_id, PocPlayer player)
+    {
+        _players.TryAdd(connection_id, player);
+    }
+
+    public void exit(string connection_id)
+    {
+        _players.TryRemove(connection_id, out _);
+    }
+}
